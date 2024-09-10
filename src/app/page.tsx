83a536a18448900e2af2dcfcc22325a256d34ff1
jhs512 <jangka512@gmail.com>
2024-09-11 00:44:22 +0900

    work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsToEye } from "@fortawesome/free-solid-svg-icons/faArrowsToEye";
+
 export default function Home() {
-  return <div>안녕하세요.</div>;
+  return (
+    <div className="text-blue-500">
+      안녕하세요. <FontAwesomeIcon icon={faArrowsToEye} />
+    </div>
+  );
 }
